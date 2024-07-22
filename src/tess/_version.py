1c54830bde7de45f45ce52cb0501c0a7b265682e
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-22 12:49:33 +0200

    first asyncio routines

--- src/tess/_version.py
+++ src/tess/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g330256d.d20240722'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g330256d.d20240722')
+__version__ = version = '0.1.dev3+gb301450.d20240722'
+__version_tuple__ = version_tuple = (0, 1, 'dev3', 'gb301450.d20240722')
--- src/tess/ida/main.py
+++ src/tess/ida/main.py
@@ -10,18 +10,22 @@
 
 import os
 import sys
+import asyncio
 import logging
 import datetime
+import itertools
 
 # -------------------
 # Third party imports
 # -------------------
 
 import decouple
-import requests
+import aiohttp
+import aiofiles
+
 from dateutil.relativedelta import relativedelta
 
-from lica.cli import execute
+from lica.cli import async_execute
 from lica.validators import vmonth, vyear
 
 #--------------
@@ -47,40 +51,55 @@ log = logging.getLogger(__name__.split('.')[-1])
 # Auxiliary functions
 # -------------------
 
-def do_ida_single_month(base_url: str, name: str, month: str, specific: bool = False) -> None:
-    url = base_url + '/download'
-    target_file = name + '_' + month + '.dat' if not specific else month
-    params = {'path': '/' + name, 'files': target_file}
-    resp = requests.get(url, params=params)
-    if resp.status_code == 404:
-        log.warn("No monthly file exits: %s", target_file)
-        return
-    log.info("GET %s [%d OK]", resp.url, resp.status_code)
-    resp.raise_for_status() # catch other unexpected return code
+def grouper(n, iterable):
+    iterable = iter(iterable)
+    return iter(lambda: list(itertools.islice(iterable, n)), [])
+
+
+def mkdir(name: str, filename: str, base_dir: str | None) -> str:
+    base_dir = os.getcwd() if base_dir is None else base_dir
     dir_path = os.path.join(os.getcwd(), name)
     if not os.path.isdir(dir_path):
         log.debug("new directory: %s", dir_path)
         os.mkdir(dir_path)
-    file_path = os.path.join(dir_path, target_file)
-    with open(file_path,'w') as f:
+    return os.path.join(dir_path, filename)
+
+
+async def do_ida_single_month(session, base_url: str, name: str, month: str, specific: bool = False) -> None:
+    url = base_url + '/download'
+    target_file = name + '_' + month + '.dat' if not specific else month
+    params = {'path': '/' + name, 'files': target_file}
+    async with session.get(url, params=params) as resp:
+        if resp.status == 404:
+            log.warn("No monthly file exits: %s", target_file)
+            return
+        log.info("GET %s [%d OK]", resp.url, resp.status)
+        contents = await resp.text()
+    file_path = await asyncio.to_thread(mkdir, name, target_file, base_dir=None)
+    async with aiofiles.open(file_path, mode='w') as f:
         log.info("writing %s", file_path)
-        f.write(resp.text)
+        await f.write(contents)
 
-def ida_single_month(base_url: str, args) -> None:
+
+async def ida_single_month(base_url: str, args) -> None:
     name = args.name
-    if args.exact:
-        do_ida_single_month(base_url, name, args.exact, specific=True)
-    else:
-        month = args.month.strftime('%Y-%m')
-        do_ida_single_month(base_url, name, month)
+    async with aiohttp.ClientSession() as session:
+        if args.exact:
+            await do_ida_single_month(session, base_url, name, args.exact, specific=True)
+        else:
+            month = args.month.strftime('%Y-%m')
+            await do_ida_single_month(session, base_url, name, month)
 
-def ida_year(base_url: str, args) -> None:
+async def ida_year(base_url: str, args) -> None:
     year = args.year.replace(month=1, day=1)
     name = args.name
-    url = base_url + '/download'
-    for i in range(0,12):
-        cur_month = year + relativedelta(months=i)
-        do_ida_single_month(base_url, name, cur_month.strftime('%Y-%m'))
+    async with aiohttp.ClientSession() as session:
+        for grp in grouper(4, range(0,12)):
+            log.info("Dealing with months %s",grp)
+            tasks = [asyncio.create_task(do_ida_single_month(session, base_url, name, 
+                (year + relativedelta(months=m)).strftime('%Y-%m'))) for m in grp]
+            await asyncio.gather(*tasks)
+
 
 def ida_since(base_url: str, args) -> None:
     name = args.name
@@ -102,24 +121,6 @@ def ida_all(base_url: str, args) -> None:
             cur_month = M1.strftime('%Y-%m')
             do_ida_single_month(base_url, name, cur_month)
             M1 += relativedelta(months=1)
-
-CMD_TABLE = {
-    'month': ida_single_month,
-    'year': ida_year,
-    'since': ida_since,
-    'all': ida_all,
-}
-
-# -------------
-# MAIN FUNCTION
-# -------------
-
-def get_ida(args):
-    '''The main entry point specified by pyproject.toml'''
-    base_url = decouple.config('IDA_URL')
-    func = CMD_TABLE[args.command]
-    func(base_url, args)
-    log.info("done!")
 
 # ===================================
 # MAIN ENTRY POINT SPECIFIC ARGUMENTS
@@ -149,12 +150,29 @@ def add_args(parser):
     parser_all.add_argument('-s', '--since',  type=vmonth, required=True, metavar='<YYYY-MM>', help='Year and Month')
     parser_all.add_argument('-u', '--until',  type=vmonth, default=now(), metavar='<YYYY-MM>', help='Year and Month (defaults to current month)')
     return parser
-    
+
+# ================    
 # MAIN ENTRY POINT
 # ================
 
+
+CMD_TABLE = {
+    'month': ida_single_month,
+    'year': ida_year,
+    'since': ida_since,
+    'all': ida_all,
+}
+
+async def get_ida(args):
+    '''The main entry point specified by pyproject.toml'''
+    base_url = decouple.config('IDA_URL')
+    func = CMD_TABLE[args.command]
+    await func(base_url, args)
+    log.info("done!")
+
+
 def main():
-    execute(main_func=get_ida, 
+    async_execute(main_func=get_ida, 
         add_args_func=add_args, 
         name=__name__, 
         version=__version__,
