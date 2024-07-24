6100a9b5aae6e4eac2b026187e3fd076c3d73e02
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-24 22:24:56 +0200

    playing with constants

--- src/tess/ida/constants.py
+++ src/tess/ida/constants.py
@@ -13,17 +13,34 @@ from enum import Enum
 class BaseEnum(Enum):
     @classmethod
     def values(cls):
-        return [member.name for _, member in cls.__members__.items()]
+        return [member.value for _, member in cls.__members__.items()]
 
 class TW(BaseEnum):
-    TIME = 'UTC Date & Time'
+    UTC_TIME   = 'time'
     LOCAL_TIME = 'Local Date & Time'
-    BOX_TEMP = 'Enclosure Temperature'
-    SKY_TEMP = 'Sky Temperature'
-    FREQ1 = 'Frequency'
-    MAG1 = 'MSAS'
-    ZP1 = 'ZP'
-    SEQ = 'Sequence Number'
+    BOX_TEMP   = 'Enclosure Temperature'
+    SKY_TEMP   = 'Sky Temperature'
+    FREQ1      = 'Frequency'
+    MAG1       = 'MSAS'
+    ZP1        = 'ZP'
+    SEQ        = 'Sequence Number'
 
+class T4C(BaseEnum):
+    UTC_TIME   = 'time'
+    LOCAL_TIME = 'Local Date & Time'
+    BOX_TEMP   = 'Enclosure Temperature'
+    SKY_TEMP   = 'Sky Temperature'
+    FREQ1      = 'Freq2'
+    MAG1       = 'MSAS1'
+    ZP1        = 'ZP1'
+    FREQ2      = 'Freq2'
+    MAG2       = 'MSAS2'
+    ZP2        = 'ZP2'
+    FREQ3      = 'Freq3'
+    MAG3       = 'MSAS3'
+    ZP3        = 'ZP3'
+    FREQ4      = 'Freq4'
+    MAG4       = 'MSAS4'
+    ZP4        = 'ZP4'
+    SEQ        = 'Sequence Number'
 
-IDA_EXCLUDE =(TESSCol.LOCAL_TIME,)
