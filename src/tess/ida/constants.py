ae494eb343dcb6a38a71afec6a33e02bf5e1c03f
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-24 22:16:38 +0200

    changing stuff

--- src/tess/ida/constants.py
+++ src/tess/ida/constants.py
@@ -9,3 +9,21 @@
 # -----------------------
 
 from enum import Enum
+
+class BaseEnum(Enum):
+    @classmethod
+    def values(cls):
+        return [member.name for _, member in cls.__members__.items()]
+
+class TW(BaseEnum):
+    TIME = 'UTC Date & Time'
+    LOCAL_TIME = 'Local Date & Time'
+    BOX_TEMP = 'Enclosure Temperature'
+    SKY_TEMP = 'Sky Temperature'
+    FREQ1 = 'Frequency'
+    MAG1 = 'MSAS'
+    ZP1 = 'ZP'
+    SEQ = 'Sequence Number'
+
+
+IDA_EXCLUDE =(TESSCol.LOCAL_TIME,)
